12c9a78221611567927860947e25e1830cf1d03e
Peter Ilberg <peter.ilberg@gmail.com>
2026-05-08 16:49:24 +0200

    Copy usb_ethernet.rs example from embassy/examples/rp.

---
+++ src/main.rs
@@ -0,0 +1,269 @@
+//! "~/git/embassy/examples/rp/src/bin/usb_ethernet.rs" 212L, 7599B
+//! This example shows how to use USB (Universal Serial Bus) in the RP2040 chip.
+//!
+//! This is a CDC-NCM class implementation, aka Ethernet over USB.
+
+#![no_std]
+#![no_main]
+
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_net::tcp::TcpSocket;
+use embassy_net::udp::{PacketMetadata, UdpMetadata, UdpSocket};
+use embassy_net::{
+    DhcpConfig, IpEndpoint, Ipv4Address, Ipv4Cidr, Stack, StackResources, StaticConfigV4,
+};
+use embassy_rp::clocks::RoscRng;
+use embassy_rp::peripherals::USB;
+use embassy_rp::usb::{Driver, InterruptHandler};
+use embassy_rp::{bind_interrupts, peripherals};
+use embassy_usb::class::cdc_acm::{CdcAcmClass, State as AcmState};
+use embassy_usb::class::cdc_ncm::embassy_net::{Device, Runner, State as NetState};
+use embassy_usb::class::cdc_ncm::{CdcNcmClass, State as NcmState};
+use embassy_usb::{Builder, Config, UsbDevice};
+use embedded_io_async::Write;
+use heapless::Vec;
+use leasehund::{
+    DHCPServerBuffers, DHCPServerSocket, DhcpConfig as LeaseConfig, DhcpConfigBuilder, DhcpServer,
+    TransactionEvent,
+};
+use static_cell::StaticCell;
+use {defmt_rtt as _, panic_probe as _};
+
+bind_interrupts!(struct Irqs {
+    USBCTRL_IRQ => InterruptHandler<USB>;
+});
+
+type MyDriver = Driver<'static, peripherals::USB>;
+
+const MTU: usize = 1514;
+
+#[embassy_executor::task]
+async fn usb_task(mut device: UsbDevice<'static, MyDriver>) -> ! {
+    device.run().await
+}
+
+#[embassy_executor::task]
+async fn usb_ncm_task(class: Runner<'static, MyDriver, MTU>) -> ! {
+    class.run().await
+}
+
+#[embassy_executor::task]
+async fn net_task(mut runner: embassy_net::Runner<'static, Device<'static, MTU>>) -> ! {
+    runner.run().await
+}
+
+#[embassy_executor::task]
+async fn log_task(logger: CdcAcmClass<'static, Driver<'static, USB>>) -> ! {
+    // Creates the logger and returns the logger future
+    // Note: You'll need to use log::info! afterwards instead of info! for this to work (this also applies to all the other log::* macros)
+    let log_fut = embassy_usb_logger::with_class!(1024, log::LevelFilter::Info, logger);
+    log_fut.await
+}
+
+#[embassy_executor::task]
+async fn dhcp_task(mut server: DhcpServer<32, 4>, stack: Stack<'static>) -> ! {
+    let mut buffers = DHCPServerBuffers::new();
+    let mut socket = DHCPServerSocket::new(stack, &mut buffers);
+    loop {
+        let Ok(event) = server.lease_one(&mut socket).await else {
+            // Handle error (e.g., log it)
+            log::info!("Lease failed.");
+            continue;
+        };
+
+        match event {
+            TransactionEvent::Leased(ip, mac) => {
+                log::info!("Leased IP: {} to MAC: {:02x?}", ip, mac);
+                let config = stack.config_v4().unwrap();
+                stack.set_config_v4(embassy_net::ConfigV4::Static(StaticConfigV4 {
+                    gateway: Some(ip),
+                    ..config
+                }));
+                stack.wait_config_up().await;
+                log::info!("Replace gateway");
+            }
+            TransactionEvent::Released(ip, mac) => {
+                log::info!("Released IP: {} from MAC: {:02x?}", ip, mac);
+            }
+        }
+    }
+}
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    let p = embassy_rp::init(Default::default());
+    let mut rng = RoscRng;
+
+    // Create the driver, from the HAL.
+    let driver = Driver::new(p.USB, Irqs);
+
+    // Create embassy-usb Config
+    let mut config = Config::new(0xc0de, 0xcafe);
+    config.manufacturer = Some("Embassy");
+    config.product = Some("USB-Ethernet example");
+    config.serial_number = Some("12345678");
+    config.max_power = 100;
+    config.max_packet_size_0 = 64;
+
+    // Create embassy-usb DeviceBuilder using the driver and config.
+    static CONFIG_DESC: StaticCell<[u8; 256]> = StaticCell::new();
+    static BOS_DESC: StaticCell<[u8; 256]> = StaticCell::new();
+    static CONTROL_BUF: StaticCell<[u8; 128]> = StaticCell::new();
+    let mut builder = Builder::new(
+        driver,
+        config,
+        &mut CONFIG_DESC.init([0; 256])[..],
+        &mut BOS_DESC.init([0; 256])[..],
+        &mut [], // no msos descriptors
+        &mut CONTROL_BUF.init([0; 128])[..],
+    );
+
+    // Our MAC addr.
+    let our_mac_addr = [0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC];
+    // Host's MAC addr. This is the MAC the host "thinks" its USB-to-ethernet adapter has.
+    let host_mac_addr = [0x88, 0x88, 0x88, 0x88, 0x88, 0x88];
+
+    // Create classes on the builder.
+    static STATE: StaticCell<NcmState> = StaticCell::new();
+    let class = CdcNcmClass::new(&mut builder, STATE.init(NcmState::new()), host_mac_addr, 64);
+
+    // Create a class for the logger
+    static LOG_STATE: StaticCell<AcmState> = StaticCell::new();
+    let logger_class = CdcAcmClass::new(&mut builder, LOG_STATE.init(AcmState::new()), 64);
+
+    // Build the builder.
+    let usb = builder.build();
+
+    spawner.spawn(unwrap!(usb_task(usb)));
+    spawner.spawn(unwrap!(log_task(logger_class)));
+
+    static NET_STATE: StaticCell<NetState<MTU, 4, 4>> = StaticCell::new();
+    let (runner, device) =
+        class.into_embassy_net_device::<MTU, 4, 4>(NET_STATE.init(NetState::new()), our_mac_addr);
+    spawner.spawn(unwrap!(usb_ncm_task(runner)));
+
+    //let hostname = String::<32/*MAX_HOSTNAME_LEN*/>::try_from("picoRIO").unwrap();
+    //let mut dhcp_config = DhcpConfig::default();
+    //dhcp_config.hostname = Some(hostname);
+    // let config = embassy_net::Config::dhcpv4(DhcpConfig::default());
+    let config = embassy_net::Config::ipv4_static(embassy_net::StaticConfigV4 {
+        address: Ipv4Cidr::new(Ipv4Address::new(192, 168, 7, 1), 24),
+        dns_servers: Vec::new(),
+        gateway: Some(Ipv4Address::new(192, 168, 7, 2)),
+    });
+
+    // Generate random seed
+    let seed = rng.next_u64();
+
+    // Init network stack
+    static RESOURCES: StaticCell<StackResources<10>> = StaticCell::new();
+    let (stack, runner) =
+        embassy_net::new(device, config, RESOURCES.init(StackResources::new()), seed);
+    log::info!("waiting for config up");
+    stack.wait_config_up().await;
+
+    let config: LeaseConfig<4> = DhcpConfigBuilder::new()
+        .server_ip(Ipv4Address::new(192, 168, 7, 1))
+        .subnet_mask(Ipv4Address::new(255, 255, 255, 0))
+        .router(Ipv4Address::new(192, 168, 7, 2))
+        .add_dns_server(Ipv4Address::new(1, 1, 1, 1)) // Cloudflare DNS
+        .add_dns_server(Ipv4Address::new(1, 0, 0, 1)) // Cloudflare backup
+        .add_dns_server(Ipv4Address::new(8, 8, 8, 8)) // Google DNS
+        .ip_pool(
+            Ipv4Address::new(192, 168, 7, 100),
+            Ipv4Address::new(192, 168, 7, 200),
+        )
+        .lease_time(7200) // 2 hours
+        .build();
+
+    let dhcp_server: DhcpServer<32, 4> = DhcpServer::with_config(config);
+    spawner.spawn(unwrap!(dhcp_task(dhcp_server, stack)));
+    spawner.spawn(unwrap!(net_task(runner)));
+
+    // And now we can use it!
+
+    let mut rx_buffer = [0; 4096];
+    let mut tx_buffer = [0; 4096];
+    let mut buf = [0; 4096];
+
+    let mut udp_rx_buffer = [0; 4096];
+    let mut udp_tx_buffer = [0; 4096];
+    let mut udp_rx_meta = [PacketMetadata::EMPTY; 8];
+    let mut udp_tx_meta = [PacketMetadata::EMPTY; 8];
+
+    loop {
+        let mut socket = TcpSocket::new(stack, &mut rx_buffer, &mut tx_buffer);
+        //socket.set_timeout(Some(embassy_time::Duration::from_secs(10)));
+
+        log::info!("Listening on TCP:1234...");
+        if let Err(e) = socket.accept(1234).await {
+            log::info!("accept error: {:?}", e);
+            continue;
+        }
+
+        log::info!("Received connection from {:?}", socket.remote_endpoint());
+        log::info!("local local_endpoint {:?}", socket.local_endpoint());
+
+        loop {
+            let n = match socket.read(&mut buf).await {
+                Ok(0) => {
+                    log::info!("read EOF");
+                    break;
+                }
+                Ok(n) => n,
+                Err(e) => {
+                    log::info!("read error: {:?}", e);
+                    break;
+                }
+            };
+
+            for (i, x) in buf[..n].iter().enumerate() {
+                log::info!("rxd {} {}", i, x);
+            }
+
+            match socket.write_all(&buf[..n]).await {
+                Ok(()) => {}
+                Err(e) => {
+                    log::info!("write error: {:?}", e);
+                    break;
+                }
+            };
+
+            let mut udp = UdpSocket::new(
+                stack,
+                &mut udp_rx_meta,
+                &mut udp_rx_buffer,
+                &mut udp_tx_meta,
+                &mut udp_tx_buffer,
+            );
+            let local_endpoint = IpEndpoint::new(
+                embassy_net::IpAddress::Ipv4(Ipv4Address::new(192, 168, 7, 1)),
+                12345,
+            );
+            // let local_endpoint = IpEndpoint::new(embassy_net::IpAddress::Ipv4(Ipv4Address::UNSPECIFIED), 12345);
+            match udp.bind(local_endpoint) {
+                Ok(()) => {
+                    log::info!("bound to {}", local_endpoint);
+                }
+                Err(e) => {
+                    log::info!("bind {:?}", e);
+                }
+            }
+            let remote_endpoint = IpEndpoint::new(
+                embassy_net::IpAddress::Ipv4(Ipv4Address::new(192, 168, 64, 47)),
+                12345,
+            );
+            match udp.send_to(&buf[..n], remote_endpoint).await {
+                Ok(()) => {
+                    log::info!("sent to {}", remote_endpoint);
+                }
+                Err(e) => {
+                    log::info!("send_to {:?}", e)
+                }
+            }
+            udp.flush().await;
+            log::info!("flushed udp socket");
+        }
+    }
+}
