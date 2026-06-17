250091f2eb3d41f9af3f5d8182ef36f87051ff2e
Peter Ilberg <peter.ilberg@gmail.com>
2026-06-17 11:39:33 +0200

    Rename Control to Config.

--- pico/src/bar_graph.rs
+++ pico/src/bar_graph.rs
@@ -24,13 +24,13 @@ pub async fn select(pin: u8) {
 }
 
 #[embassy_executor::task]
-pub async fn task(mut control: Control) {
+pub async fn task(mut config: Config) {
     network::wait_for_network().await;
 
     let mut selection = None;
     let mut measurements = [0_u8; NUM_PINS];
 
-    shift_value(&mut control, get_value(&selection, &measurements)).await;
+    shift_value(&mut config, get_value(&selection, &measurements)).await;
 
     loop {
         match INBOX.receive().await {
@@ -47,7 +47,7 @@ pub async fn task(mut control: Control) {
                 measurements = new_values;
             }
         }
-        shift_value(&mut control, get_value(&selection, &measurements)).await;
+        shift_value(&mut config, get_value(&selection, &measurements)).await;
     }
 }
 
@@ -59,7 +59,7 @@ fn get_value(selection: &Option<u8>, measurements: &[u8]) -> u8 {
 }
 
 /// The bar graph is powered by an 8-bit shift register of type 74HC595.
-pub struct Control {
+pub struct Config {
     /// The latch to update the register, called "RCLK" in the datasheet.
     pub latch: Output<'static>,
     /// The clock to shift a bit, called "SRCLK" in the datasheet.
@@ -68,7 +68,7 @@ pub struct Control {
     pub data: Output<'static>,
 }
 
-async fn shift_value(control: &mut Control, value: u8) {
+async fn shift_value(config: &mut Config, value: u8) {
     // Input ranges from 0 - 100, output is a bar graph with 8 LEDs.
     // Scale input down to 0 - 8 and convert to bit pattern:
     //      0 -> 0b_0000_0000
@@ -79,18 +79,18 @@ async fn shift_value(control: &mut Control, value: u8) {
     //      8 -> 0b_1111_1111
     let mut bits = (1_u32 << (value / 12)) - 1;
 
-    control.latch.set_low();
+    config.latch.set_low();
 
     // Shift bits from lowest to highest.
     for _ in 0..8 {
         static LEVELS: [Level; 2] = [Level::Low, Level::High];
-        control.data.set_level(LEVELS[(bits & 1) as usize]);
+        config.data.set_level(LEVELS[(bits & 1) as usize]);
         bits >>= 1;
 
-        control.clock.set_high();
+        config.clock.set_high();
         embassy_time::Timer::after_micros(1).await;
-        control.clock.set_low();
+        config.clock.set_low();
     }
 
-    control.latch.set_high();
+    config.latch.set_high();
 }
